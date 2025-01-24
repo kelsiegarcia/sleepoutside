47f2d2d7c19fe22e597bee7cef377790f4f3a954
Kelsie Garcia <kelsie.c.garcia@gmail.com>
2025-01-24 08:58:23 -1000

    added public folders with contents from src

---
+++ public/js/product.js
@@ -0,0 +1,27 @@
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import { findProductById } from "./productData.mjs";
+
+function addProductToCart(product) {
+  let cart = getLocalStorage("so-cart");
+
+  // Ensure cart is always an array
+  if (!Array.isArray(cart)) {
+    cart = [];
+  }
+
+  // Add product to cart
+  cart.push(product);
+
+  setLocalStorage("so-cart", cart);
+}
+
+// Add to cart button event handler
+async function addToCartHandler(e) {
+  const product = await findProductById(e.target.dataset.id);
+  addProductToCart(product);
+}
+
+// Add listener to Add to Cart button
+document
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
