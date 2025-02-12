06cbe112497b5e6182550357d7bdb7ab19f4a306
Caitlyn Caldwell <cecldwll@gmail.com>
2025-02-11 17:13:09 -0700

    team 5 step 2

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,4 @@ import { productList } from "./productList.mjs";
 import { loadHeaderFooter } from "../js/utils.mjs";
 
 loadHeaderFooter();
+productList(".product-list", "tents");
--- src/js/product-list.js
+++ src/js/product-list.js
@@ -1,11 +1,9 @@
 import { productList } from "./productList.mjs";
-import { loadHeaderFooter } from "../js/utils.mjs";
+import { getParam,loadHeaderFooter } from "../js/utils.mjs";
 
-// Load header and footer
 loadHeaderFooter();
 
-// Get the category from the URL, if available, otherwise default to "tents"
-const category = new URLSearchParams(window.location.search).get('category') || 'tents';
+const category = getParam("category");
 
-// Call the productList function to display products based on the category
+// call the productList function to display products based on the category
 productList(".product-list", category);
