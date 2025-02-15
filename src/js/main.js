7877328fa271fc2641dadbea9097fc6f5ffd0a35
Gláucio Oliveira <glaucio.dev@gmail.com>
2025-02-15 02:17:47 -0300

    removing code from main.js, adding product list title,  fix on images and product detail url

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,3 @@
-import { productList } from "./productList.mjs";
 import { loadHeaderFooter } from "../js/utils.mjs";
 
 loadHeaderFooter();
-productList(".product-list", "tents");
--- src/js/product-list.js
+++ src/js/product-list.js
@@ -5,5 +5,7 @@ loadHeaderFooter();
 
 const category = getParam("category");
 
+document.querySelector(".title").innerText = category;
+
 // call the productList function to display products based on the category
 productList(".product-list", category);
