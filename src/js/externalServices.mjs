6f7d74ef375aecb48a76703dda0b399a45d1ebaf
Kelsie Garcia <kelsie.c.garcia@gmail.com>
2025-03-08 10:16:33 -1000

    small url fix

--- src/js/externalServices.mjs
+++ src/js/externalServices.mjs
@@ -32,18 +32,20 @@ export async function checkout(payload) {
   return await fetch(baseURL + "/checkout/", options).then(convertToJson);
 }
 export async function loginRequest(creds) {
-  const response = await fetch("http://server-nodejs.cit.byui.edu:3000/login", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(creds),
-  });
+  const response = await fetch(
+    "https://server-nodejs.cit.byui.edu:3000/login",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(creds),
+    }
+  );
 
   if (!response.ok) {
     throw new Error("Invalid credentials");
   }
 
   const data = await response.json();
-  const { accessToken } = data; 
+  const { accessToken } = data;
   return accessToken;
 }
-
