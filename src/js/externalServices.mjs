912bd2d907ff3e56b1e6e993c9e0721b3f77985b
Kelsie Garcia <kelsie.c.garcia@gmail.com>
2025-03-12 13:54:39 -1000

    ...

--- src/js/externalServices.mjs
+++ src/js/externalServices.mjs
@@ -31,21 +31,33 @@ export async function checkout(payload) {
   };
   return await fetch(baseURL + "/checkout/", options).then(convertToJson);
 }
-export async function loginRequest(creds) {
+// export async function loginRequest(creds) {
+//   const response = await fetch("http://server-nodejs.cit.byui.edu:3000/login", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify(creds),
+//   });
+
+//   if (!response.ok) {
+//     throw new Error("Invalid credentials");
+//   }
+
+//   const data = await response.json();
+//   const { accessToken } = data;
+//   return accessToken;
+// }
+
+export async function loginRequest(user) {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  };
   const response = await fetch(
-    "https://server-nodejs.cit.byui.edu:3000/login",
-    {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(creds),
-    }
-  );
-
-  if (!response.ok) {
-    throw new Error("Invalid credentials");
-  }
-
-  const data = await response.json();
-  const { accessToken } = data;
-  return accessToken;
+    "http://server-nodejs.cit.byui.edu:3000/" + "login",
+    options
+  ).then(convertToJson);
+  return response.accessToken;
 }
