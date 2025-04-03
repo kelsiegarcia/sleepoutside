cbc937e70d5a1c94dc2f61c143f3bd3333972bbf
Justus <galaxycheesemc@gmail.com>
2025-04-03 10:34:42 -0700

    Added cart bounce animation

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,64 +1,3 @@
-// import { findProductById } from "./externalServices.mjs";
-// import { getLocalStorage, setLocalStorage } from "./utils.mjs";
-// import { loadHeaderFooter, alertMessage } from "./utils.mjs";
-
-// let product = {};
-
-// export default async function productDetails(productId) {
-//   // get the details for the current product. findProductById will return a promise! use await or .then() to process it
-//   product = await findProductById(productId);
-//   // once we have the product details we can render out the HTML
-//   renderProductDetails();
-//   // once the HTML is rendered we can add a listener to Add to Cart button
-//   document
-//     .getElementById("addToCart")
-//     .addEventListener("click", addProductToCart);
-// }
-
-// export function addProductToCart(product) {
-//   let cartContents = getLocalStorage("so-cart");
-
-//   if (!cartContents) {
-//     cartContents = [];
-//   }
-//   // Check if product is already in cart
-//   const existingProduct = cartContents.find((item) => item.Id === product.Id);
-//   if (existingProduct) {
-//     // Increment quantity
-//     existingProduct.quantity = (existingProduct.quantity || 1) + 1;
-//   } else {
-//     // Add product to cart
-//     product.quantity = 1;
-//     cartContents.push(product);
-//   }
-
-//   setLocalStorage("so-cart", cartContents);
-//   alertMessage(`${product.NameWithoutBrand} added to cart!`);
-// }
-
-// // Add to cart button event handler
-// async function addToCartHandler(e) {
-//   const product = await findProductById(e.target.dataset.id);
-//   addProductToCart(product);
-// }
-
-// // Render product details
-// function renderProductDetails() {
-//   document.querySelector("#productName").innerText = product.Brand.Name;
-//   document.querySelector("#productNameWithoutBrand").innerText =
-//     product.NameWithoutBrand;
-//   document.querySelector("#productImage").src = product.Images.PrimaryLarge;
-//   document.querySelector("#productImage").alt = product.Name;
-//   document.querySelector("#productFinalPrice").innerText = product.FinalPrice;
-//   document.querySelector("#productColorName").innerText =
-//     product.Colors[0].ColorName;
-//   document.querySelector("#productDescriptionHtmlSimple").innerHTML =
-//     product.DescriptionHtmlSimple;
-//   document.querySelector("#addToCart").dataset.id = product.Id;
-// }
-
-// loadHeaderFooter();
-
 import { findProductById } from "./externalServices.mjs";
 import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import { loadHeaderFooter, alertMessage } from "./utils.mjs";
@@ -79,7 +18,7 @@ export default async function productDetails(productId) {
 }
 
 export function addProductToCart(product) {
-  let cartContents = getLocalStorage("so-cart") || []; // Default empty array
+  let cartContents = getLocalStorage("so-cart") || []; 
 
   const existingProduct = cartContents.find((item) => item.Id === product.Id);
 
@@ -91,8 +30,20 @@ export function addProductToCart(product) {
   }
 
   setLocalStorage("so-cart", cartContents);
+  animateCartIcon(); 
   alertMessage(`${product.NameWithoutBrand} added to cart!`);
 }
+
+function animateCartIcon() {
+  const cartIcon = document.querySelector(".cart"); 
+  if (cartIcon) {
+    cartIcon.classList.add("cart-bounce");
+    setTimeout(() => {
+      cartIcon.classList.remove("cart-bounce");
+    }, 500); 
+  }
+}
+
 
 function renderProductDetails(product) {
   document.querySelector("#productName").innerText = product.Brand.Name;
